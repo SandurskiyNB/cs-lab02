c0196871435cb0b151433d4886ce1fedc3e3b358
Alice <alice@example.net>
2023-07-09 02:09:47 +0300

    code: добавлен вывод максимума

--- lab02.cpp
+++ lab02.cpp
@@ -8,4 +8,8 @@ int main() {
 		<< "A - B = " << a - b << "\n"
 		<< "A * B = " << a * b << "\n"
 		<< "A / B = " << a / b << "\n";
+	if (a > b)
+		cout << "max = " << a;
+	else
+		cout << "max = " << b;
 }
